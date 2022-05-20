6cac8e69ef2e7af2b10d4131e7dabe2c406912f1
Abbas <160197415@aston.ac.uk>
2022-05-20 15:11:59 +0100

    reworked, old work is in comments

--- src/test/java/TileTest.java
+++ src/test/java/TileTest.java
@@ -5,13 +5,13 @@ public class TileTest {
 
     // Name of Class is : test___________
 
-    Tile tile = new Tile();
+    Tile tile =new Tile();
 
     @Test
     public void testSetTile() {
 
-        tile.setiValue(2);
-        Assertions.assertEquals(2, tile.getiValue(), "Incorrect i value");
+
+        Assertions.assertEquals("x", tile.getDisplay(), "Incorrect item displayed");
 
     }
 }
